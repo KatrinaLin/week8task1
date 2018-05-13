563382d21e443087e39bd3e18f1733245632842c
Katrina <zhaoyanlin2016@gmail.com>
2018-05-13 18:11:27 +0800

    feat: complete task for retrieve the company list or a specific company

--- src/main/java/com/example/employee/restfulapi/controller/CompanyController.java
+++ src/main/java/com/example/employee/restfulapi/controller/CompanyController.java
@@ -1,5 +1,33 @@
 package com.example.employee.restfulapi.controller;
 
+import com.example.employee.restfulapi.entity.Company;
+import com.example.employee.restfulapi.repository.CompanyRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+import static org.springframework.web.bind.annotation.RequestMethod.GET;
+
+@RestController
+@RequestMapping(value = "/companies")
 public class CompanyController {
+
+    @Autowired
+    private CompanyRepository companyRepository;
+
     //在此处完成Company API
+    @RequestMapping(method = GET)
+    public List<Company> getCompanies() {
+        return companyRepository.findAll();
+    }
+
+    @RequestMapping(value = "/{id}", method = GET)
+    public Company getCompanyWithId(@PathVariable(value="id") Long companyId) {
+        return companyRepository.findOne(companyId);
+    }
+
+
 }
